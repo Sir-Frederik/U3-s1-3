513e200924a94bfa894f506096fe9f6b0ad11a59
Federico <brunetti.federico.13@gmail.com>
2025-05-15 13:54:00 +0200

    riaggiunti i bottoni

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,45 @@ import Footer from "./components/Footer";
 import Welcome from "./components/Welcome";
 import fantasy from "./data/fantasy.json";
 import BookList from "./components/BookList";
-/* import scifi from "./data/scifi.json";
+import scifi from "./data/scifi.json";
 import history from "./data/history.json";
 import romance from "./data/romance.json";
 import horror from "./data/horror.json";
- */
+import { Component } from "react";
+import { Button } from "react-bootstrap";
+
 // import BookList from "./components/AllTheBooks";
 
-function App() {
-  return (
-    <>
-      <MyNav brandName="Ciccio pasty"></MyNav>
-      <Welcome alertColor="info" alertContent="Ciaooooo!" textContent="Come stai?" />
-      <BookList genre={fantasy} />
-      <Footer />
-    </>
-  );
+class App extends Component {
+  state = {
+    collection: scifi,
+  };
+  render() {
+    return (
+      <>
+        <MyNav brandName="Libreria Alì BlaBla"></MyNav>
+        <div className="d-flex gap-1 my-2">
+          <Button variant="primary" onClick={() => this.setState({ collection: scifi })}>
+            Scifi
+          </Button>
+          <Button variant="success" onClick={() => this.setState({ collection: fantasy })}>
+            Fantasy
+          </Button>
+          <Button variant="danger" onClick={() => this.setState({ collection: history })}>
+            History
+          </Button>
+          <Button variant="dark" onClick={() => this.setState({ collection: horror })}>
+            Horror
+          </Button>
+          <Button variant="warning" onClick={() => this.setState({ collection: romance })}>
+            Romance
+          </Button>
+        </div>
+        <BookList genre={this.state.collection} />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
