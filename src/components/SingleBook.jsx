3bfd715beb1ce6fbd5b7aac8def4635545b32b66
Federico <brunetti.federico.13@gmail.com>
2025-05-15 11:19:47 +0200

    aggiunsto stato selected in singleBook

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,20 +1,27 @@
 import Card from "react-bootstrap/Card";
 
 import { Button, Col } from "react-bootstrap";
+import { Component } from "react";
 
-const SingleBook = (props) => {
-  return (
-    <Col key={props.book.asin}>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={props.book.img} />
-        <Card.Body variant="secondary">
-          <Card.Title>{props.book.title}</Card.Title>
-          <Card.Text>{props.book.category}</Card.Text>
-          <Card.Text>{props.book.price}$</Card.Text>
-          <Button variant="primary">Acquista</Button>
-        </Card.Body>
-      </Card>
-    </Col>
-  );
-};
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  };
+  render() {
+    return (
+      <Col key={this.props.book.asin}>
+        <Card className="book-cover d-flex flex-column" style={{ width: "18rem" }}>
+          {/* mi salvo lo stato opposto dfi quello che trova, con !this.state.selected  */}
+          <Card.Img variant="top" src={this.props.book.img} onClick={() => this.setState({ selected: !this.state.selected })} />
+          <Card.Body variant="secondary">
+            <Card.Title>{this.props.book.title}</Card.Title>
+            <Card.Text>{this.props.book.category}</Card.Text>
+            <Card.Text>{this.props.book.price}$</Card.Text>
+            <Button variant="primary">Acquista</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  }
+}
 export default SingleBook;
